af7f5d901ef279a9234f41c220a25c17a5e3a5bd
Kevin <kevin.bruno.work@gmail.com>
2026-01-20 12:15:46 +0100

    OpenRouteService added

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 
-PROJECT_ROOT = Path(__file__).resolve().parents[1]
+PACKAGE_ROOT = Path(__file__).resolve().parents[1]
 
-DATA_PATH = PROJECT_ROOT / "data"
-MODELS_PATH = PROJECT_ROOT / "model_development"
+DATA_PATH = PACKAGE_ROOT / "data"
+MODELS_PATH = PACKAGE_ROOT / "model_development"
 
 TAXI_CSV_PATH = DATA_PATH / "taxi_trip_pricing.csv"
+TAXI_CLEANED_CSV = MODELS_PATH / "taxi_cleaned_training_data.csv"
 MODEL_PATH = MODELS_PATH / "taxi_price_model.joblib"
